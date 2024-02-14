55b876f316db87ecd7c53d05decfb5f5a61e40a2
beattris <beatriceebirim@gmail.com>
2024-02-14 15:25:17 +0100

    fixed a bug to make the modal work

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
+
 import { SharedModule } from '../shared/shared.module';
-
-
 
 @NgModule({
   declarations: [
